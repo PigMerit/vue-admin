c97a5f88260298abc88f585bc7757931091a5a7f
merit <whu@liuzhenpeng.com>
2020-09-09 16:49:34 +0800

    第一次提交，创建了基本的项目，使用了路由并按需引入了elementUI

---
+++ src/views/login/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">
+    <h1>{{ num }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num: 100
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
